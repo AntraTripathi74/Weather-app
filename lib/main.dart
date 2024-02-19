a8fd9698ac7d95362b28da363eff9c2ed8f99874
Antra Tripathi <tripathiantra074@gmail.com>
2024-02-19 17:48:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_app/weather_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
@@ -15,7 +19,18 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(useMaterial3: true)
           .copyWith(appBarTheme: const AppBarTheme()),
-      home: const WeatherScreen(),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (contex, snapshot) {
+          if (snapshot.hasError) {
+            print('Error');
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const WeatherScreen();
+          }
+          return const CircularProgressIndicator.adaptive();
+        },
+      ),
     );
   }
 }
